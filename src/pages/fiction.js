025218f7b593fbdf2f3975d21b0684f2115260ce
Tim Beckett <timbeckett@ymail.com>
2019-04-25 04:14:03 -0400

    added fiction and non-fiction pages, updated header.

---
+++ src/pages/fiction.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+
+
+import Layout from '../components/layout';
+
+const FictionPage = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        allWordpressPost ( filter: { categories: {elemMatch: {
+            name: { eq: "fiction" }
+        }}}) {
+        edges {
+            node {
+                date (formatString:"MMMM Do, YYYY")
+                title
+                slug
+                categories {
+                    name
+                }
+                tags {
+                    name
+                }
+            }
+        }
+    }
+    }
+`)
+    return (
+        <Layout>
+            <section>
+                <h1>Fiction Page</h1>
+                <ul>
+                    {
+                        data.allWordpressPost.edges.map((post) => {
+                            return (
+                                <li className="post-link">
+
+
+                                    <Link to={post.node.slug}>
+                                        <h2 dangerouslySetInnerHTML={{ __html: post.node.title }} />
+                                    </Link>
+
+                                    <p>{post.node.date}</p>
+
+                                </li>
+
+                            )
+                        })
+                    }
+
+                </ul>
+            </section>
+        </Layout>
+    )
+}
+
+export default FictionPage; 
---
+++ src/pages/non-fiction.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+const NonFictionPage = () => {
+    return (
+        <Layout>
+            <section>
+                <h1>Non Fiction Page</h1>
+            </section>
+        </Layout>
+    )
+}
+
+export default NonFictionPage; 
