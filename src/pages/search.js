3fc69b542d70e6184969984ccbd4bb90e665f275
Tim Beckett <timbeckett@ymail.com>
2019-06-01 10:29:47 -0400

    changed all.js ro search.js for better seo.

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
-import SearchPage from '../components/search/all';
+import SearchPage from '../components/search/search';
 
 const ListSearchPage = () => {
     return (
         <Layout>
             <section>
-                <h1>All Posts</h1>
                 <SearchPage></SearchPage>
             </section>
         </Layout>
